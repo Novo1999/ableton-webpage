8a966447028a68cea4c37c703bc3daf780d38fe1
Novodip Mondal <neptune.navo@gmail.com>
2023-08-04 01:04:06 +0600

    able

--- src/components/ImageSection/ImageSection.jsx
+++ src/components/ImageSection/ImageSection.jsx
@@ -1,30 +1,42 @@
 import styles from "./ImageSection.module.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function ImageSection() {
   return (
-    <div className={styles.image__section}>
-      <div className={styles.studio__text}>
-        <h1>Your computer is your instrument</h1>
-        <a href="https://www.ableton.com/en/live/">
-          Learn more about Live &gt;
-        </a>
+    <section className={styles.image__section}>
+      <div className={styles.studio}>
+        <div className={styles.studio_link}>
+          <h1>Your computer is your instrument</h1>
+          <Link to="Live">Learn more about Live &gt;</Link>
+        </div>
+        <img
+          className={styles.studio__img}
+          src="/images/pexels-pixabay-164938-min.jpg"
+          alt="studio"
+        />
       </div>
-      <img
-        className={styles.studio}
-        src="/images/pexels-pixabay-164938-min.jpg"
-        alt="studio"
-      />
-      <img
-        className={styles.habit}
-        src="/images/pexels-lisa-fotios-1092671-min.jpg"
-        alt="habit"
-      />
-      <img
-        className={styles.music}
-        src="/images/denisse-leon-OVEWbIgffDk-unsplash-min.jpg"
-        alt="music"
-      />
-    </div>
+      <div className={styles.habit}>
+        <div className={styles.habit_link}>
+          <h1>Your computer is your instrument</h1>
+          <Link to="Live">Learn more about Live &gt;</Link>
+        </div>
+        <img
+          className={styles.habit__img}
+          src="/images/pexels-lisa-fotios-1092671-min.jpg"
+          alt="habit"
+        />
+      </div>
+      <div className={styles.music}>
+        <div className={styles.music_link}>
+          <h1>Your computer is your instrument</h1>
+          <Link to="Live">Learn more about Live &gt;</Link>
+        </div>
+        <img
+          className={styles.music__img}
+          src="/images/denisse-leon-OVEWbIgffDk-unsplash-min.jpg"
+          alt="music"
+        />
+      </div>
+    </section>
   );
 }
 
